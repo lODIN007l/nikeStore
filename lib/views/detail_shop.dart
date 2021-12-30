e0ab2ddde74b643a2c41e5b1d4293cf32f918b6b
Wilson Lluilema <wilsonlluilema007@gmail.com>
2021-12-30 18:52:25 -0500

    envio de home completo

--- lib/views/detail_shop.dart
+++ lib/views/detail_shop.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:nike_design_app/model/shoes.dart';
 
-class DetallePage extends StatelessWidget {
+class DetallePage extends StatefulWidget {
+  final Deportivo deportivo;
+
+  const DetallePage({Key? key, required this.deportivo}) : super(key: key);
+
+  @override
+  State<DetallePage> createState() => _DetallePageState();
+}
+
+class _DetallePageState extends State<DetallePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:nike_design_app/model/data.dart';
+import 'package:nike_design_app/views/detail_shop.dart';
 import 'package:nike_design_app/widgets/custom_appbar.dart';
 import 'package:nike_design_app/widgets/custom_bottom_bar.dart';
 
@@ -11,13 +12,56 @@ class HomeShop extends StatefulWidget {
 
 class _HomeShopState extends State<HomeShop> {
   final _pageController = PageController();
+  double _paginaActual = 0.0;
+  double indexPage = 0.0;
+  void _listener() {
+    setState(() {
+      _paginaActual = _pageController.page!;
+
+      if (_paginaActual >= 0 && _paginaActual < 0.7) {
+        indexPage = 0;
+      } else if (_paginaActual > 0.7 && _paginaActual < 1.7) {
+        indexPage = 1;
+      } else if (_paginaActual > 1.7 && _paginaActual < 2.7) {
+        indexPage = 2;
+      }
+    });
+  }
+
+  Color getColor() {
+    late Color color = Colors.yellow;
+    if (_paginaActual >= 0 && _paginaActual < 0.7) {
+      color = listZ[0].listaI[0].color;
+    } else if (_paginaActual > 0.7 && _paginaActual < 1.7) {
+      color = listZ[1].listaI[0].color;
+    } else if (_paginaActual > 1.7 && _paginaActual < 2.7) {
+      color = listZ[2].listaI[0].color;
+    }
+
+    return color;
+  }
+
+  @override
+  void initState() {
+    _pageController.addListener(_listener);
+
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          CustomBar(),
+          const CustomBar(),
           SizedBox(
               height: 50,
               child: Padding(
@@ -29,10 +73,12 @@ class _HomeShopState extends State<HomeShop> {
                             padding: const EdgeInsets.only(right: 12),
                             child: Text(
                               listaC[index],
-                              style: const TextStyle(
+                              style: TextStyle(
                                   fontSize: 25,
                                   fontWeight: FontWeight.w800,
-                                  color: Colors.white),
+                                  color: index == indexPage
+                                      ? getColor()
+                                      : Colors.white),
                             ),
                           )),
                 ),
@@ -45,108 +91,124 @@ class _HomeShopState extends State<HomeShop> {
                   itemBuilder: (context, index) {
                     final deportivo = listZ[index];
                     final listTitle = deportivo.categoria.split(' ');
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 60, left: 50),
-                      child: Transform.translate(
-                        offset: Offset(10, 0),
-                        child: LayoutBuilder(builder: (context, constraints) {
-                          return Container(
-                              margin: const EdgeInsets.symmetric(
-                                  vertical: 30, horizontal: 10),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Colors.white),
-                              child: Stack(
-                                clipBehavior: Clip.none,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 20.0, vertical: 40),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.stretch,
-                                      children: [
-                                        Text(
-                                          deportivo.categoria,
-                                          style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.w500),
-                                        ),
-                                        const SizedBox(
-                                          height: 8,
-                                        ),
-                                        Text(
-                                          deportivo.nombre,
-                                          style: const TextStyle(
-                                              fontSize: 28,
-                                              fontWeight: FontWeight.w800,
-                                              color: Colors.black),
-                                        ),
-                                        const SizedBox(
-                                          height: 4,
-                                        ),
-                                        Text(
-                                          deportivo.precio,
-                                          style: const TextStyle(
-                                              fontSize: 23,
-                                              fontWeight: FontWeight.w600,
-                                              color: Colors.black),
-                                        ),
-                                        const SizedBox(
-                                          height: 4,
-                                        ),
-                                        FittedBox(
-                                          child: Text(
-                                            '${listTitle[0]} \n ${listTitle[1]}    ',
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          PageRouteBuilder(
+                            pageBuilder: (context, animation, _) {
+                              return DetallePage(
+                                deportivo: deportivo,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            right: index == indexPage ? 30 : 60, left: 50),
+                        child: Transform.translate(
+                          offset: const Offset(5, 0),
+                          child: LayoutBuilder(builder: (context, constraints) {
+                            return AnimatedContainer(
+                                duration: const Duration(milliseconds: 250),
+                                margin: EdgeInsets.only(
+                                    top: index == indexPage ? 30 : 50,
+                                    bottom: 30),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: Colors.white),
+                                child: Stack(
+                                  clipBehavior: Clip.none,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20.0, vertical: 40),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.stretch,
+                                        children: [
+                                          Text(
+                                            deportivo.categoria,
                                             style: const TextStyle(
-                                                color: Colors.grey,
-                                                fontWeight: FontWeight.bold),
+                                                color: Colors.black,
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.w500),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Positioned(
-                                      top: constraints.maxHeight * 0.2,
-                                      left: constraints.maxWidth * 0.01,
-                                      right: -constraints.maxWidth * 0 - 16,
-                                      bottom: constraints.maxHeight * 0.2,
-                                      child: Image(
-                                          image: AssetImage(
-                                              deportivo.listaI[0].image))),
-                                  Positioned(
-                                      bottom: 0,
-                                      right: 0,
-                                      child: Material(
-                                        color: deportivo.listaI[0].color,
-                                        borderRadius: BorderRadius.only(
-                                            topLeft: Radius.circular(36),
-                                            bottomRight: Radius.circular(30)),
-                                        clipBehavior:
-                                            Clip.antiAliasWithSaveLayer,
-                                        child: InkWell(
-                                          onTap: () {},
-                                          child: SizedBox(
-                                            height: 100,
-                                            width: 100,
-                                            child: Icon(
-                                              Icons.add,
-                                              size: 40,
+                                          const SizedBox(
+                                            height: 8,
+                                          ),
+                                          Text(
+                                            deportivo.nombre,
+                                            style: const TextStyle(
+                                                fontSize: 28,
+                                                fontWeight: FontWeight.w800,
+                                                color: Colors.black),
+                                          ),
+                                          const SizedBox(
+                                            height: 4,
+                                          ),
+                                          Text(
+                                            deportivo.precio,
+                                            style: const TextStyle(
+                                                fontSize: 23,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.black),
+                                          ),
+                                          const SizedBox(
+                                            height: 4,
+                                          ),
+                                          FittedBox(
+                                            child: Text(
+                                              '${listTitle[0]} \n ${listTitle[1]}    ',
+                                              style: const TextStyle(
+                                                  color: Colors.grey,
+                                                  fontWeight: FontWeight.bold),
                                             ),
                                           ),
-                                        ),
-                                      ))
-                                ],
-                              ));
-                        }),
+                                        ],
+                                      ),
+                                    ),
+                                    Positioned(
+                                        top: constraints.maxHeight * 0.2,
+                                        left: constraints.maxWidth * 0.01,
+                                        right: -constraints.maxWidth * 0 - 16,
+                                        bottom: constraints.maxHeight * 0.2,
+                                        child: Image(
+                                            image: AssetImage(
+                                                deportivo.listaI[0].image))),
+                                    Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        child: Material(
+                                          color: deportivo.listaI[0].color,
+                                          borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(36),
+                                              bottomRight: Radius.circular(30)),
+                                          clipBehavior:
+                                              Clip.antiAliasWithSaveLayer,
+                                          child: InkWell(
+                                            onTap: () {},
+                                            child: SizedBox(
+                                              height: 100,
+                                              width: 100,
+                                              child: Icon(
+                                                Icons.add,
+                                                size: 40,
+                                              ),
+                                            ),
+                                          ),
+                                        ))
+                                  ],
+                                ));
+                          }),
+                        ),
                       ),
                     );
                   })),
           Container(
             height: 120,
             padding: const EdgeInsets.all(20),
-            child: BotonPersonalizado(color: listZ[0].listaI[0].color),
+            child: BotonPersonalizado(color: getColor()),
           )
         ],
       ),
