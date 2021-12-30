367d8edaac6d6ede0bd13d72b3a9b8be5b86b72b
Wilson Lluilema <wilsonlluilema007@gmail.com>
2021-12-30 06:43:03 -0500

    home casi terminado

--- lib/model/data.dart
+++ lib/model/data.dart
@@ -4,13 +4,13 @@ import 'package:nike_design_app/model/shoes.dart';
 final listaC = ['Basketball', 'Correr', 'Entrenar'];
 
 final listZ = [
-  Deportivo('AIR JORDAN 1 MID SE GC ', 'NIKE AIR ', '\$ 120', 4, [
+  Deportivo('AIR JORDAN 1 MID SE GC', 'NIKE AIR ', '\$ 120', 4, [
     ImagenDeportivo('assets/J_001.png', const Color(0xffEAA906)),
     ImagenDeportivo('assets/J_002.png', const Color(0xff08B894)),
     ImagenDeportivo('assets/J_003.png', const Color(0xffB50D0D)),
     ImagenDeportivo('assets/J_004.png', const Color(0xff229954)),
   ]),
-  Deportivo('NIKE BLAZER MID  ', 'NIKE AIR ', '\$ 150', 3, [
+  Deportivo('NIKE BLAZER MID', 'NIKE AIR 2', '\$ 150', 3, [
     ImagenDeportivo('assets/Z_001.png', const Color(0xff08B894)),
     ImagenDeportivo('assets/Z_002.png', const Color(0xffE90311)),
     ImagenDeportivo('assets/Z_003.png', const Color(0xff189E03)),
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:nike_design_app/model/data.dart';
 import 'package:nike_design_app/widgets/custom_appbar.dart';
+import 'package:nike_design_app/widgets/custom_bottom_bar.dart';
 
-class HomeShop extends StatelessWidget {
+class HomeShop extends StatefulWidget {
+  @override
+  State<HomeShop> createState() => _HomeShopState();
+}
+
+class _HomeShopState extends State<HomeShop> {
   final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,19 +44,110 @@ class HomeShop extends StatelessWidget {
                   itemCount: listZ.length,
                   itemBuilder: (context, index) {
                     final deportivo = listZ[index];
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 30, horizontal: 10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white),
-                      child: Text(
-                        deportivo.categoria,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.black, fontSize: 22),
+                    final listTitle = deportivo.categoria.split(' ');
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 60, left: 50),
+                      child: Transform.translate(
+                        offset: Offset(10, 0),
+                        child: LayoutBuilder(builder: (context, constraints) {
+                          return Container(
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 30, horizontal: 10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.white),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20.0, vertical: 40),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      children: [
+                                        Text(
+                                          deportivo.categoria,
+                                          style: const TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 22,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                        const SizedBox(
+                                          height: 8,
+                                        ),
+                                        Text(
+                                          deportivo.nombre,
+                                          style: const TextStyle(
+                                              fontSize: 28,
+                                              fontWeight: FontWeight.w800,
+                                              color: Colors.black),
+                                        ),
+                                        const SizedBox(
+                                          height: 4,
+                                        ),
+                                        Text(
+                                          deportivo.precio,
+                                          style: const TextStyle(
+                                              fontSize: 23,
+                                              fontWeight: FontWeight.w600,
+                                              color: Colors.black),
+                                        ),
+                                        const SizedBox(
+                                          height: 4,
+                                        ),
+                                        FittedBox(
+                                          child: Text(
+                                            '${listTitle[0]} \n ${listTitle[1]}    ',
+                                            style: const TextStyle(
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Positioned(
+                                      top: constraints.maxHeight * 0.2,
+                                      left: constraints.maxWidth * 0.01,
+                                      right: -constraints.maxWidth * 0 - 16,
+                                      bottom: constraints.maxHeight * 0.2,
+                                      child: Image(
+                                          image: AssetImage(
+                                              deportivo.listaI[0].image))),
+                                  Positioned(
+                                      bottom: 0,
+                                      right: 0,
+                                      child: Material(
+                                        color: deportivo.listaI[0].color,
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(36),
+                                            bottomRight: Radius.circular(30)),
+                                        clipBehavior:
+                                            Clip.antiAliasWithSaveLayer,
+                                        child: InkWell(
+                                          onTap: () {},
+                                          child: SizedBox(
+                                            height: 100,
+                                            width: 100,
+                                            child: Icon(
+                                              Icons.add,
+                                              size: 40,
+                                            ),
+                                          ),
+                                        ),
+                                      ))
+                                ],
+                              ));
+                        }),
                       ),
                     );
-                  }))
+                  })),
+          Container(
+            height: 120,
+            padding: const EdgeInsets.all(20),
+            child: BotonPersonalizado(color: listZ[0].listaI[0].color),
+          )
         ],
       ),
     );
