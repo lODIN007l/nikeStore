2f5ba69fd1b4d2b16c55169b984c463177c8e6a5
Wilson Lluilema <wilsonlluilema007@gmail.com>
2021-12-30 05:49:58 -0500

    home page en progreso

--- lib/model/data.dart
+++ lib/model/data.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:nike_design_app/model/shoes.dart';
 
 final listaC = ['Basketball', 'Correr', 'Entrenar'];
 
 final listZ = [
-  //Shoes(
-  'AIR JORDAN 1 MID SE GC ',
-  'NIKE AIR ',
-  '\$ 120',
-  4,
-  [Image]
-
-  //)
+  Deportivo('AIR JORDAN 1 MID SE GC ', 'NIKE AIR ', '\$ 120', 4, [
+    ImagenDeportivo('assets/J_001.png', const Color(0xffEAA906)),
+    ImagenDeportivo('assets/J_002.png', const Color(0xff08B894)),
+    ImagenDeportivo('assets/J_003.png', const Color(0xffB50D0D)),
+    ImagenDeportivo('assets/J_004.png', const Color(0xff229954)),
+  ]),
+  Deportivo('NIKE BLAZER MID  ', 'NIKE AIR ', '\$ 150', 3, [
+    ImagenDeportivo('assets/Z_001.png', const Color(0xff08B894)),
+    ImagenDeportivo('assets/Z_002.png', const Color(0xffE90311)),
+    ImagenDeportivo('assets/Z_003.png', const Color(0xff189E03)),
+    ImagenDeportivo('assets/Z_004.png', const Color(0xffF76A02)),
+  ]),
+  Deportivo('ZoomX Vaporfly', 'NIKE ZOOM', '\$ 180', 3, [
+    ImagenDeportivo('assets/N_001.png', const Color(0xff7B7A41)),
+    ImagenDeportivo('assets/N_002.png', const Color(0xff2C2C46)),
+    ImagenDeportivo('assets/N_003.png', const Color(0xff262626)),
+    ImagenDeportivo('assets/N_004.png', const Color(0xff5C9462)),
+  ])
 ];
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:nike_design_app/model/data.dart';
+import 'package:nike_design_app/widgets/custom_appbar.dart';
 
 class HomeShop extends StatelessWidget {
+  final _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('olaa'),
-    ));
+      body: Column(
+        children: [
+          CustomBar(),
+          SizedBox(
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Row(
+                  children: List.generate(
+                      listaC.length,
+                      (index) => Padding(
+                            padding: const EdgeInsets.only(right: 12),
+                            child: Text(
+                              listaC[index],
+                              style: const TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white),
+                            ),
+                          )),
+                ),
+              )),
+          Expanded(
+              child: PageView.builder(
+                  controller: _pageController,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: listZ.length,
+                  itemBuilder: (context, index) {
+                    final deportivo = listZ[index];
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 30, horizontal: 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white),
+                      child: Text(
+                        deportivo.categoria,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black, fontSize: 22),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
   }
 }
